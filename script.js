4877fae3542b40eff0eacb766494b52978b175d2
alizaLakhani14 <49196299+alizaLakhani14@users.noreply.github.com>
2019-06-15 17:06:30 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,12 @@
+// function Age(){
+//     let reqAge = document.querySelector ("input").value; 
+
+
+// if(reqAge > 14){
+//     alert("Eligible");
+// } 
+
+// else{
+//     alert("Sorry not eligible");
+// }
+// }
